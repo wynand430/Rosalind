34b2b285995240fd2174b1444dfa140aac3cea8e
Du Plessis, Carl (cd9584) <cd9584@us.att.com>
2017-12-08 20:54:59 -0600

    Finish Mortal Fibonaci Rabbitsgit status

--- FIBD/Mortal_Fibonacci_Rabbits.py
+++ FIBD/Mortal_Fibonacci_Rabbits.py
@@ -1,73 +1,76 @@
 import EasyFiles as ef 
 
 input = ef.getInput() # get the input list
-munth, lifespan = input[0].split(' ')
-munth = int(munth)
+duration, lifespan = input[0].split(' ')
+duration = int(duration)
 lifespan = int(lifespan)
+print "d:%d l:%d" % (duration, lifespan)
 
 # Given: Positive integers n<=100 and m<=20
 # Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
-#
-# input of 3 3
-# 5 = months (n)
-# 3 = liveSpan (k)
 
-### 3
-# 0 = 1
-# 1 = 0
-# 2 = [1]+[0]
-# 3 = [2]+[1]
-# 4 = [3]+[2]-[0]
-# 5 = [4]+[3]-[1]
-# 6 = [5]+[4]-[3]
-# 7 = [6]+[5]-[4]
-# 8 = [7]+[6]-[5]
+#n = months (starts at 1)
+#L = lifespan
+#born[n] = total[n-1]-born[(n-1)-L] (if n-1==0: 0)(if n-L==0: return 1; if n-L<0: return 0)
+#total[n](if n-1==0: 1) = total[n-1]+born[n-1]-born[n-L](if n-L==0: return 1; if n-L<0: return 0) 
 
-### 10
-# 0 = 1
-# 1 = 0
-# 2 = [1]+[0]
-# 3 = [2]+[1]
-# 4 = [3]+[2]
-# 5 = [4]+[3]
-# 6 = [5]+[4]
-# 7 = [6]+[5]
-# 8 = [7]+[6]
-# 9 = [8]+[7]
-# 10 = [9]+[8]
-# 11 = [10]+[9]-[0]
-# 12 = [11]+[9]-[1]
+totalDic = {}
+bornDic = {}
 
-seq =[1,0]
-
-def bunny_machine(months, lifespan):
-	global seq
-	if months < len(seq):
-		return seq[months]
-	else:
-		if months > (lifespan):
-			death_shift = bunny_machine(months-1-lifespan,lifespan)
+def storeTotal(index, res):
+	totalDic[index] = res
+	try:
+		del totalDic[index-2]
+	except:
+		pass
+def storeBorn(index, res, lifespan):
+	bornDic[index] = res
+	try:
+		del bornDic[(index-2)-lifespan]
+	except:
+		pass
+def total(index, lifespan):
+	try:
+		return totalDic[index]
+	except:
+		if index==1:
+			res = 1
 		else:
-			death_shift = 0
-		mod1 = bunny_machine(months-1,lifespan)
-		mod2 = bunny_machine(months-2,lifespan)
-		answer = mod1+mod2-death_shift
-		print "months (%d): [%d]+[%d] -[%d]= %d" % (months, mod1, mod2, death_shift, answer)
-		seq.append(answer)
-		return answer
+			res = total(index-1, lifespan)+born(index-1, lifespan)-born((index-1)-lifespan, lifespan)
+			storeTotal(index, res)
+		return res
+def born(index, lifespan):
+	try:
+		return bornDic[index]
+	except:
+		if index<0:
+			res = 0
+		elif index==0:
+			res = 1
+		elif index==1:
+			res = 0
+		else:
+			res = total(index-1, lifespan)-born((index-1)-lifespan, lifespan)
+			storeBorn(index, res, lifespan)
+		return res
+
+ans = total(duration, lifespan)
+print "ans: %d" % ans
+ef.writeOutput( str( ans ) )
 
 
-bunny_machine(munth,lifespan)
-print seq[munth]
 
-
-
-
-
-
-
-
-
-ef.writeOutput( str( seq[munth] ) )
-
-# fib()
+'''
+Helpful Examples:
+25, 31 => 75025
+30, 30 => 832040
+35, 29 => 9227437
+40, 28 => 102333267
+45, 27 => 1134880302
+50, 26 => 12585734257
+55, 25 => 139572033844
+60, 24 => 1547756440188
+65, 23 => 17162433794472
+70, 22 => 190285454264264
+75, 21 => 2109333161649033
+'''
